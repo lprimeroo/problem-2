9a933b17d4dedfa1e8de2edd12405ef5c298f6c5
Sarthak Munshi <sarthakmunshi@gmail.com>
2018-10-25 19:15:18 +0530

    Refactored script and ran script on 15 files

--- dataset/2.c
+++ dataset/2.c
@@ -5,8 +5,14 @@ int main()
  
    printf("Enter number of elements\n");
    scanf("%d",&n);
-   
+
    if (first > last)
+      printf("Not found! %d isn't present in the list.\n", search);
+    
+  if (first > last)
+      printf("Not found! %d isn't present in the list.\n", search);
+
+  if (first > last)
       printf("Not found! %d isn't present in the list.\n", search);
  
    return 0;  
